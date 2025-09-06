9efc93a74ca07cc6fdb5dc77c34ded9f3a29ffad
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2025-09-06 18:56:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ecommercemern-frontend2.onrender.com",
+    origin: "www.imadsilk.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
