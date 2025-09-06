41754085fbb94715b2f0193aa6ee94f83e812966
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2025-09-06 15:39:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
